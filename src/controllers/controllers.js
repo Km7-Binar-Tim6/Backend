98b2df32a0b031b819dda9d8326cecfae1f0dac9
CaseStudyProgramming <casestudyprogramming@gmail.com>
2024-10-17 21:30:53 +0700

    fitur manufakture

---
+++ src/controllers/controllers.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const controllers = () => {
+  return (
+    <View>
+      <Text>controllers</Text>
+    </View>
+  );
+};
+
+export default controllers;
